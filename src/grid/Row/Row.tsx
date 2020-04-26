fa8f8d08435daccd8d0e36ec5fe3d736212f8e05
ricardo <ricardo-svc@live.com>
2020-04-26 02:16:59 -0300

    Adjusting row properties

--- src/grid/Row/Row.tsx
+++ src/grid/Row/Row.tsx
@@ -30,15 +30,21 @@ interface Props {
 
 const getResponsive = (name: string, val?: Responsives) => {
   const align = typeof val === 'object' && val.align ? val.align : val;
+  const justify = typeof val === 'object' && val.justify ? val.justify : val;
+  const reverse = typeof val === 'object' && val.reverse ? val.reverse : val;
 
   return {
-    [`${name}-${align}`]: align && typeof align !== 'object'
+    [`${name}-${align}`]: align && typeof align !== 'object',
+    [`${name}-${justify}`]: justify && typeof align !== 'object',
+    [`${name}-${reverse}`]: reverse && typeof reverse !== 'object'
   };
 };
 
-const Row = ({ children, align, reverse, sm, md, lg }: Props) => {
+const Row = ({ children, justify, align, reverse, sm, md, lg }: Props) => {
   const classes = classNames('row', {
     [`align-${align}`]: align,
+    [`justify-${justify}`]: justify,
+    [`reverse-${reverse}`]: reverse,
     ...getResponsive('sm', sm),
     ...getResponsive('md', md),
     ...getResponsive('lg', lg),
